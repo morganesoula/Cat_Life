16c6d2e2fbdb62f4442d6121158b49fe31d10bd5
Morgane Soula <morganes56@gmail.com>
2023-12-08 11:46:34 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+        maven { url  = uri("https://jitpack.io") }
+    }
+}
+rootProject.name = "Cat Life"
+include ("com.msoula.catlife:app")
